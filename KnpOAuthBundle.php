1d7406efcbcd920d4d2652b80fdf7291da6945cf
Geoffrey Bachelet <grosfrais@gmail.com>
2011-12-21 11:54:11 +0100

    factories are now loaded through the bundle's build method

--- KnpOAuthBundle.php
+++ KnpOAuthBundle.php
@@ -3,8 +3,16 @@
 namespace Knp\Bundle\OAuthBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Knp\Bundle\OAuthBundle\DependencyInjection\Security\Factory\OAuthFactory;
 
 class KnpOAuthBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
 
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new OAuthFactory());
+    }
 }
