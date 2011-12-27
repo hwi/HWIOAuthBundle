37ff31326e9a0c385120caf501bd6e945714f15f
Joseph Bielawski <stloyd@gmail.com>
2011-12-27 12:54:17 +0100

    [GitHub] Use API v3 URL for accessing user data

--- Security/Http/OAuth/GithubProvider.php
+++ Security/Http/OAuth/GithubProvider.php
@@ -26,7 +26,7 @@ class GithubProvider extends OAuthProvider
     protected $options = array(
         'authorization_url' => 'https://github.com/login/oauth/authorize',
         'access_token_url'  => 'https://github.com/login/oauth/access_token',
-        'infos_url'         => 'https://github.com/api/v2/json/user/show',
+        'infos_url'         => 'https://api.github.com/user',
         'username_path'     => 'user.login',
     );
 
