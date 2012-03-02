asm89/KnpOAuthBundle

Initial implementation allowing multiple resource owners in one firewall

Controller/SecurityController.php
@@ -1,0 +1,97 @@
+<?php
+
+/*
+ * This file is part of the KnpOAuthBundle package.
+ *
+ * (c) KnpLabs <hello@knplabs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Knp\Bundle\OAuthBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use HWI\SocialConnectBundle\Security\Exception\SocialAccountNotLinkedException;
+
+/**
+ * SecurityController
+ *
+ * @author Alexander <iam.asm89@gmail.com>
+ */
+class SecurityController extends ContainerAware
+{
+    /**
+     * Login action showing links to all the resource owners. defined for a
+     * firewall.
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function loginAction(Request $request)
+    {
+        if (!$this->container->getParameter('knp_oauth.firewall_name')) {
+            throw new \RuntimeException('A firewall_name should be configured in order to use the default login action.');
+        }
+
+        $ownerMap = $this->container->get('knp_oauth.resource_ownermap.'.$this->container->getParameter('knp_oauth.firewall_name'));
+        $httpUtils = $this->container->get('security.http_utils');
+
+        $resourceOwners = array();
+        foreach ($ownerMap->getResourceOwners() as $resourceOwnerInfo) {
+            $resourceOwner = $this->container->get($resourceOwnerInfo['service']);
+            $resourceOwners[] = array(
+                'url' => $resourceOwner->getAuthorizationUrl($this->getUriForCheckPath($request, $resourceOwnerInfo['check_path'])),
+                'name' => $resourceOwnerInfo['service'],
+            );
+        }
+
+        $session = $request->getSession();
+
+        // code adapted from FOSUB: https://github.com/FriendsOfSymfony/FOSUserBundle/blob/2c7721/Controller/SecurityController.php
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        if ($error) {
+            // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
+            $error = $error->getMessage();
+        }
+
+        return $this->container->get('templating')->renderResponse('KnpOAuthBundle:Security:login.html.twig', array(
+            'error'         => $error,
+            'resource_owners' => $resourceOwners,
+        ));
+    }
+
+    /**
+     * Get the uri for a given path.
+     *
+     * @param Request $request A request instance
+     * @param string  $path    Path or route
+     *
+     * @return string
+     */
+    private function getUriForCheckPath(Request $request, $path)
+    {
+        if ($path && '/' !== $path[0] && 0 !== strpos($path, 'http')) {
+            $path = $this->container('router')->generate($path, array(), true);
+        }
+
+        if (0 !== strpos($path, 'http')) {
+            $path = $request->getUriForPath($path);
+        }
+
+        return $path;
+    }
+}

Security/Http/Firewall/OAuthListener.php
@@ -9,15 +9,15 @@
  * file that was distributed with this source code.
  */
 
-namespace Knp\Bundle\OauthBundle\Security\Http\Firewall;
+namespace Knp\Bundle\OAuthBundle\Security\Http\Firewall;
 
 use Symfony\Component\Security\Http\Firewall\AbstractAuthenticationListener,
     Symfony\Component\HttpFoundation\Request,
     Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 
-use Knp\Bundle\OAuthBundle\OAuth\ResourceOwnerInterface,
-    Knp\Bundle\OAuthBundle\Security\Core\Authentication\Token\OAuthToken;
+use Knp\Bundle\OAuthBundle\Security\Core\Authentication\Token\OAuthToken,
+    Knp\Bundle\OAuthBundle\Security\Http\ResourceOwnerMap;
 
 /**
  * OAuthListener
@@ -28,23 +28,44 @@ use Knp\Bundle\OAuthBundle\OAuth\ResourceOwnerInterface,
 class OAuthListener extends AbstractAuthenticationListener
 {
     /**
-     * @var Knp\Bundle\OAuthBundle\OAuth\ResourceOwnerInterface
+     * @var ResourceOwnerMap
      */
-    private $resourceOwner;
-
-    private $checkPath;
+    private $resourceOwnerMap;
 
     /**
-     * @var Knp\Bundle\OAuthBundle\OAuth\ResourceOwnerInterface $resourceOwner
+     * @var array
      */
-    public function setResourceOwner(ResourceOwnerInterface $resourceOwner)
+    private $checkPaths;
+
+    /**
+     * @var ResourceOwnerMap $resourceOwnerMap
+     */
+    public function setResourceOwnerMap(ResourceOwnerMap $resourceOwnerMap)
     {
-        $this->resourceOwner = $resourceOwner;
+        $this->resourceOwnerMap = $resourceOwnerMap;
     }
 
-    public function setCheckPath($checkPath)
+    /**
+     * @param array $checkPaths
+     */
+    public function setCheckPaths(array $checkPaths)
     {
-        $this->checkPath = $checkPath;
+        $this->checkPaths = $checkPaths;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function requiresAuthentication(Request $request)
+    {
+        // Check if the route matches one of the check paths
+        foreach ($this->checkPaths as $checkPath) {
+            if ($this->httpUtils->checkRequestPath($request, $checkPath)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
@@ -52,12 +73,15 @@ class OAuthListener extends AbstractAuthenticationListener
      */
     protected function attemptAuthentication(Request $request)
     {
-        $accessToken = $this->resourceOwner->getAccessToken(
+        list($resourceOwner, $checkPath, $resourceOwnerId) = $this->resourceOwnerMap->getResourceOwnerByRequest($request);
+
+        $accessToken = $resourceOwner->getAccessToken(
             $request->query->get('code'),
-            $this->httpUtils->createRequest($request, $this->checkPath)->getUri()
+            $this->httpUtils->createRequest($request, $checkPath)->getUri()
         );
 
         $token = new OAuthToken($accessToken);
+        $token->setResourceOwnerId($resourceOwnerId);
 
         return $this->authenticationManager->authenticate($token);
     }

Security/Http/ResourceOwnerMap.php
@@ -1,0 +1,103 @@
+<?php
+
+/*
+ * This file is part of the KnpOAuthBundle package.
+ *
+ * (c) KnpLabs <hello@knplabs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Knp\Bundle\OAuthBundle\Security\Http;
+
+use Symfony\Component\DependencyInjection\ContainerInterface,
+    Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\Security\Core\Authentication\Token\TokenInterface,
+    Symfony\Component\Security\Http\HttpUtils;
+
+/**
+ * ResourceOwnerMap. Holds several resource owners for a firewall. Lazy
+ * loads the appropriate resource owner when requested.
+ *
+ * @author Alexander <iam.asm89@gmail.com>
+ */
+class ResourceOwnerMap
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @var HttpUtils
+     */
+    protected $httpUtils;
+
+    /**
+     * @var array
+     */
+    protected $resourceOwners;
+
+    /**
+     * Constructor.
+     *
+     * @param ContainerInterface $container
+     * @param HttpUtils          $httpUtils
+     */
+    public function __construct(ContainerInterface $container, HttpUtils $httpUtils)
+    {
+        $this->container = $container;
+        $this->httpUtils = $httpUtils;
+    }
+
+    /**
+     * Add a resource owner to the map.
+     *
+     * @param string $resourceOwnerId
+     * @param array  $configuration
+     */
+    public function addResourceOwner($resourceOwnerId, array $configuration)
+    {
+        $configuration['service'] = $resourceOwnerId;
+        $this->resourceOwners[$configuration['service']] = $configuration;
+    }
+
+    /**
+     * Gets the appropriate resource owner given the id.
+     *
+     * @param string $id
+     *
+     * @return null|Knp\Bundle\OAuthBundle\OAuthResourceOwnerInterface
+     */
+    public function getResourceOwnerById($id)
+    {
+        return isset($this->resourceOwners[$id]) ? $this->container->get($id) : null;
+    }
+
+    /**
+     * Gets the appropriate resource owner for a request.
+     *
+     * @param Request $request
+     *
+     * @return null|Knp\Bundle\OAuthBundle\OAuthResourceOwnerInterface
+     */
+    public function getResourceOwnerByRequest(Request $request)
+    {
+        foreach ($this->resourceOwners as $resourceOwner) {
+            if ($this->httpUtils->checkRequestPath($request, $resourceOwner['check_path'])) {
+                return array($this->container->get($resourceOwner['service']), $resourceOwner['check_path'], $resourceOwner['service']);
+            }
+        }
+    }
+
+    /**
+     * Get all the resource owners.
+     *
+     * @return array
+     */
+    public function getResourceOwners()
+    {
+        return $this->resourceOwners;
+    }
+}

hwi/HWIOAuthBundle

Controller/SecurityController.php
@@ -1,0 +1,96 @@
+<?php
+
+/*
+ * This file is part of the HWIOAuthBundle package.
+ *
+ * (c) Hardware.Info <opensource@hardware.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HWI\Bundle\OAuthBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+
+/**
+ * SecurityController
+ *
+ * @author Alexander <iam.asm89@gmail.com>
+ */
+class SecurityController extends ContainerAware
+{
+    /**
+     * Login action showing links to all the resource owners. defined for a
+     * firewall.
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function loginAction(Request $request)
+    {
+        if (!$this->container->getParameter('hwi_oauth.firewall_name')) {
+            throw new \RuntimeException('A firewall_name should be configured in order to use the default login action.');
+        }
+
+        $ownerMap = $this->container->get('hwi_oauth.resource_ownermap.'.$this->container->getParameter('hwi_oauth.firewall_name'));
+        $httpUtils = $this->container->get('security.http_utils');
+
+        $resourceOwners = array();
+        foreach ($ownerMap->getResourceOwners() as $resourceOwnerInfo) {
+            $resourceOwner = $this->container->get($resourceOwnerInfo['service']);
+            $resourceOwners[] = array(
+                'url' => $resourceOwner->getAuthorizationUrl($this->getUriForCheckPath($request, $resourceOwnerInfo['check_path'])),
+                'name' => $resourceOwnerInfo['service'],
+            );
+        }
+
+        $session = $request->getSession();
+
+        // code adapted from FOSUB: https://github.com/FriendsOfSymfony/FOSUserBundle/blob/2c7721/Controller/SecurityController.php
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        if ($error) {
+            // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
+            $error = $error->getMessage();
+        }
+
+        return $this->container->get('templating')->renderResponse('HWIOAuthBundle:Security:login.html.twig', array(
+            'error'         => $error,
+            'resource_owners' => $resourceOwners,
+        ));
+    }
+
+    /**
+     * Get the uri for a given path.
+     *
+     * @param Request $request A request instance
+     * @param string  $path    Path or route
+     *
+     * @return string
+     */
+    private function getUriForCheckPath(Request $request, $path)
+    {
+        if ($path && '/' !== $path[0] && 0 !== strpos($path, 'http')) {
+            $path = $this->container('router')->generate($path, array(), true);
+        }
+
+        if (0 !== strpos($path, 'http')) {
+            $path = $request->getUriForPath($path);
+        }
+
+        return $path;
+    }
+}

Security/Http/Firewall/OAuthListener.php
@@ -16,8 +16,8 @@ use Symfony\Component\Security\Http\Firewall\AbstractAuthenticationListener,
     Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 
-use HWI\Bundle\OAuthBundle\OAuth\ResourceOwnerInterface,
-    HWI\Bundle\OAuthBundle\Security\Core\Authentication\Token\OAuthToken;
+use HWI\Bundle\OAuthBundle\Security\Core\Authentication\Token\OAuthToken,
+    HWI\Bundle\OAuthBundle\Security\Http\ResourceOwnerMap;
 
 /**
  * OAuthListener
@@ -28,23 +28,44 @@ use HWI\Bundle\OAuthBundle\OAuth\ResourceOwnerInterface,
 class OAuthListener extends AbstractAuthenticationListener
 {
     /**
-     * @var HWI\Bundle\OAuthBundle\OAuth\ResourceOwnerInterface
+     * @var ResourceOwnerMap
      */
-    private $resourceOwner;
-
-    private $checkPath;
+    private $resourceOwnerMap;
 
     /**
-     * @var HWI\Bundle\OAuthBundle\OAuth\ResourceOwnerInterface $resourceOwner
+     * @var array
      */
-    public function setResourceOwner(ResourceOwnerInterface $resourceOwner)
+    private $checkPaths;
+
+    /**
+     * @var ResourceOwnerMap $resourceOwnerMap
+     */
+    public function setResourceOwnerMap(ResourceOwnerMap $resourceOwnerMap)
     {
-        $this->resourceOwner = $resourceOwner;
+        $this->resourceOwnerMap = $resourceOwnerMap;
     }
 
-    public function setCheckPath($checkPath)
+    /**
+     * @param array $checkPaths
+     */
+    public function setCheckPaths(array $checkPaths)
     {
-        $this->checkPath = $checkPath;
+        $this->checkPaths = $checkPaths;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function requiresAuthentication(Request $request)
+    {
+        // Check if the route matches one of the check paths
+        foreach ($this->checkPaths as $checkPath) {
+            if ($this->httpUtils->checkRequestPath($request, $checkPath)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
@@ -52,12 +73,15 @@ class OAuthListener extends AbstractAuthenticationListener
      */
     protected function attemptAuthentication(Request $request)
     {
-        $accessToken = $this->resourceOwner->getAccessToken(
+        list($resourceOwner, $checkPath, $resourceOwnerId) = $this->resourceOwnerMap->getResourceOwnerByRequest($request);
+
+        $accessToken = $resourceOwner->getAccessToken(
             $request->query->get('code'),
-            $this->httpUtils->createRequest($request, $this->checkPath)->getUri()
+            $this->httpUtils->createRequest($request, $checkPath)->getUri()
         );
 
         $token = new OAuthToken($accessToken);
+        $token->setResourceOwnerId($resourceOwnerId);
 
         return $this->authenticationManager->authenticate($token);
     }

Security/Http/ResourceOwnerMap.php
@@ -1,0 +1,103 @@
+<?php
+
+/*
+ * This file is part of the HWIOAuthBundle package.
+ *
+ * (c) Hardware.Info <opensource@hardware.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HWI\Bundle\OAuthBundle\Security\Http;
+
+use Symfony\Component\DependencyInjection\ContainerInterface,
+    Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\Security\Core\Authentication\Token\TokenInterface,
+    Symfony\Component\Security\Http\HttpUtils;
+
+/**
+ * ResourceOwnerMap. Holds several resource owners for a firewall. Lazy
+ * loads the appropriate resource owner when requested.
+ *
+ * @author Alexander <iam.asm89@gmail.com>
+ */
+class ResourceOwnerMap
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @var HttpUtils
+     */
+    protected $httpUtils;
+
+    /**
+     * @var array
+     */
+    protected $resourceOwners;
+
+    /**
+     * Constructor.
+     *
+     * @param ContainerInterface $container
+     * @param HttpUtils          $httpUtils
+     */
+    public function __construct(ContainerInterface $container, HttpUtils $httpUtils)
+    {
+        $this->container = $container;
+        $this->httpUtils = $httpUtils;
+    }
+
+    /**
+     * Add a resource owner to the map.
+     *
+     * @param string $resourceOwnerId
+     * @param array  $configuration
+     */
+    public function addResourceOwner($resourceOwnerId, array $configuration)
+    {
+        $configuration['service'] = $resourceOwnerId;
+        $this->resourceOwners[$configuration['service']] = $configuration;
+    }
+
+    /**
+     * Gets the appropriate resource owner given the id.
+     *
+     * @param string $id
+     *
+     * @return null|HWI\Bundle\OAuthBundle\OAuthResourceOwnerInterface
+     */
+    public function getResourceOwnerById($id)
+    {
+        return isset($this->resourceOwners[$id]) ? $this->container->get($id) : null;
+    }
+
+    /**
+     * Gets the appropriate resource owner for a request.
+     *
+     * @param Request $request
+     *
+     * @return null|array
+     */
+    public function getResourceOwnerByRequest(Request $request)
+    {
+        foreach ($this->resourceOwners as $resourceOwner) {
+            if ($this->httpUtils->checkRequestPath($request, $resourceOwner['check_path'])) {
+                return array($this->container->get($resourceOwner['service']), $resourceOwner['check_path'], $resourceOwner['service']);
+            }
+        }
+    }
+
+    /**
+     * Get all the resource owners.
+     *
+     * @return array
+     */
+    public function getResourceOwners()
+    {
+        return $this->resourceOwners;
+    }
+}