3bbcaf63378cb9a874c89539966220db2b1c46f3
Andrew N. Shalaev <isqad88@yandex.ru>
2012-10-22 16:06:56 +0600

    fixed vkontakte api to version 2.0

--- OAuth/ResourceOwner/VkontakteResourceOwner.php
+++ OAuth/ResourceOwner/VkontakteResourceOwner.php
@@ -11,6 +11,8 @@
 
 namespace HWI\Bundle\OAuthBundle\OAuth\ResourceOwner;
 
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+
 /**
  * VkontakteResourceOwner
  *
@@ -25,6 +27,7 @@ class VkontakteResourceOwner extends GenericOAuth2ResourceOwner
         'authorization_url'   => 'https://api.vk.com/oauth/authorize',
         'access_token_url'    => 'https://oauth.vk.com/access_token',
         'infos_url'           => 'https://api.vk.com/method/getUserInfoEx',
+        'extend_info_url'     => 'https://api.vk.com/method/users.get',
         'scope'               => '',
         'user_response_class' => '\HWI\Bundle\OAuthBundle\OAuth\Response\PathUserResponse',
     );
@@ -33,9 +36,10 @@ class VkontakteResourceOwner extends GenericOAuth2ResourceOwner
      * {@inheritDoc}
      */
     protected $paths = array(
-        'identifier' => 'response.user_id',
-        'nickname'   => 'response.user_name',
-        'realname'   => 'response.user_name',
+        'identifier' => 'response.uid',
+        'nickname'   => 'response.nickname',
+        'realname'   => 'response.screen_name',
+        'profilepicture' => 'response.photo_big',
     );
 
     /**
@@ -45,5 +49,52 @@ class VkontakteResourceOwner extends GenericOAuth2ResourceOwner
     public function configure()
     {
         $this->options['scope'] = str_replace(',', ' ', $this->options['scope']);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getUserInformation($accessToken)
+    {
+        $url = $this->getOption('infos_url');
+        $url .= (false !== strpos($url, '?') ? '&' : '?').http_build_query(array(
+            'access_token' => $accessToken
+        ));
+
+        $content = $this->doGetUserInformationRequest($url)->getContent();
+
+        $response = $this->getUserResponse();
+        $response->setResponse($content);
+        $response->setResourceOwner($this);
+        $response->setAccessToken($accessToken);
+
+        $response = $response->getResponse();
+
+        //Create request for more information
+
+        $url = $this->getOption('extend_info_url');
+        $url .= (false !== strpos($url, '?') ? '&' : '?').http_build_query(array(
+            'access_token' => $accessToken
+        ));
+        $url .= '&uids=' . $response['response']['user_id'] . '&fields=first_name,last_name,nickname,screen_name,sex,photo_big';
+
+        $response = $this->getUserResponse();
+
+        $content = $this->doGetUserInformationRequest($url)->getContent();
+        $content = json_decode($content, true);
+
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            throw new AuthenticationException(sprintf('Not a valid JSON response.'));
+        }
+
+        $responseParameter = array(
+            'response' => $content['response'][0],
+        );
+
+        $response->setResponse(json_encode($responseParameter));
+        $response->setResourceOwner($this);
+        $response->setAccessToken($accessToken);
+
+        return $response;
     }
 }
