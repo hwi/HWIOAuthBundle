532822a46df965ae75e2e4621ae4d7ab6aaad271
Joseph Bielawski <stloyd@gmail.com>
2013-03-27 16:18:03 +0100

    Use API 1.1 for Twitter not the deprecated 1.0

--- Tests/OAuth/ResourceOwner/TwitterResourceOwnerTest.php
+++ Tests/OAuth/ResourceOwner/TwitterResourceOwnerTest.php
@@ -34,7 +34,7 @@ json;
                 'authorization_url'   => 'https://api.twitter.com/oauth/authenticate',
                 'request_token_url'   => 'https://api.twitter.com/oauth/request_token',
                 'access_token_url'    => 'https://api.twitter.com/oauth/access_token',
-                'infos_url'           => 'http://api.twitter.com/1/account/verify_credentials.json',
+                'infos_url'           => 'http://api.twitter.com/1.1/account/verify_credentials.json',
             ),
             $options
         );
