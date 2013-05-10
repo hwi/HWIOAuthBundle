ecc5a4cae9db4f875ed3476331b01751f44cf3d5
Joseph Bielawski <stloyd@gmail.com>
2013-05-10 09:05:11 +0200

    Cleanup odnoklassniki feature

--- Tests/OAuth/ResourceOwner/OdnoklassnikiResourceOwnerTest.php
+++ Tests/OAuth/ResourceOwner/OdnoklassnikiResourceOwnerTest.php
@@ -12,32 +12,36 @@
 namespace HWI\Bundle\OAuthBundle\Tests\OAuth\ResourceOwner;
 
 use HWI\Bundle\OAuthBundle\OAuth\ResourceOwner\OdnoklassnikiResourceOwner;
-use Symfony\Component\HttpFoundation\Request;
 
-class OdniklassnikiResourceOwnerTest extends GenericOAuth2ResourceOwnerTest
+class OdnoklassnikiResourceOwnerTest extends GenericOAuth2ResourceOwnerTest
 {
     protected $userResponse = <<<json
 {
-    "id": "1",
+    "uid": "1",
     "username": "bar"
 }
 json;
 
     protected $paths = array(
-        'identifier' => 'id',
+        'identifier' => 'uid',
         'nickname'   => 'username',
         'realname'   => 'name',
     );
+
+    public function testGetOptionAppKey()
+    {
+        $this->assertEquals('123456', $this->resourceOwner->getOption('application_key'));
+    }
 
     protected function setUpResourceOwner($name, $httpUtils, array $options)
     {
         $options = array_merge(
             array(
-                'authorization_url'         => 'http://www.odnoklassniki.ru/oauth/authorize',
-                'access_token_url'          => 'http://api.odnoklassniki.ru/oauth/token.do',
-                'infos_url'                 => 'http://api.odnoklassniki.ru/fb.do?method=users.getCurrentUser',
+                'authorization_url' => 'http://www.odnoklassniki.ru/oauth/authorize',
+                'access_token_url'  => 'http://api.odnoklassniki.ru/oauth/token.do',
+                'infos_url'         => 'http://api.odnoklassniki.ru/fb.do?method=users.getCurrentUser',
 
-                'odnoklassniki_app_key'     => '123456',
+                'application_key'   => '123456',
             ),
             $options
         );
