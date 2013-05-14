ea7e0df028ba99a79467dce7157d52330c9006b1
Alex Vasilenko <aa.vasilenko@gmail.com>
2013-05-14 15:52:51 +0300

    google resource owner options added request_visible_actions and approval_prompt support

--- OAuth/ResourceOwner/GoogleResourceOwner.php
+++ OAuth/ResourceOwner/GoogleResourceOwner.php
@@ -23,14 +23,17 @@ class GoogleResourceOwner extends GenericOAuth2ResourceOwner
      * {@inheritDoc}
      */
     protected $options = array(
-        'authorization_url'   => 'https://accounts.google.com/o/oauth2/auth',
-        'access_token_url'    => 'https://accounts.google.com/o/oauth2/token',
-        'infos_url'           => 'https://www.googleapis.com/oauth2/v1/userinfo',
+        'authorization_url'       => 'https://accounts.google.com/o/oauth2/auth',
+        'access_token_url'        => 'https://accounts.google.com/o/oauth2/token',
+        'infos_url'               => 'https://www.googleapis.com/oauth2/v1/userinfo',
 
-        'scope'               => 'https://www.googleapis.com/auth/userinfo.profile',
+        'scope'                   => 'https://www.googleapis.com/auth/userinfo.profile',
 
         // @link https://developers.google.com/accounts/docs/OAuth2WebServer#offline
-        'access_type'         => null,
+        'access_type'             => null,
+        'request_visible_actions' => null,
+        //sometimes we need to force for approval prompt (e.g. when we lost refresh token)
+        'approval_prompt'         => 'auto',
     );
 
     /**
@@ -49,6 +52,14 @@ class GoogleResourceOwner extends GenericOAuth2ResourceOwner
      */
     public function getAuthorizationUrl($redirectUri, array $extraParameters = array())
     {
-        return parent::getAuthorizationUrl($redirectUri, array_merge(array('access_type' => $this->getOption('access_type')), $extraParameters));
+        $params = array(
+            'access_type' => $this->getOption('access_type'),
+            'approval_prompt' => $this->getOption('approval_prompt')
+        );
+        if ($requestVisibleActions = $this->getOption('request_visible_actions')) {
+            $params['request_visible_actions'] = $requestVisibleActions;
+        }
+
+        return parent::getAuthorizationUrl($redirectUri, array_merge($params, $extraParameters));
     }
 }
