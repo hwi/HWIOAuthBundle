22102dd3488abb3816d0641b5c5a4abcb46a00e0
Joseph Bielawski <stloyd@gmail.com>
2013-05-15 09:17:21 +0200

    Fix typos in GoogleResourceOwner

--- OAuth/ResourceOwner/GoogleResourceOwner.php
+++ OAuth/ResourceOwner/GoogleResourceOwner.php
@@ -32,7 +32,7 @@ class GoogleResourceOwner extends GenericOAuth2ResourceOwner
         // @link https://developers.google.com/accounts/docs/OAuth2WebServer#offline
         'access_type'             => null,
         'request_visible_actions' => null,
-        //sometimes we need to force for approval prompt (e.g. when we lost refresh token)
+        // sometimes we need to force for approval prompt (e.g. when we lost refresh token)
         'approval_prompt'         => null,
     );
 
@@ -53,9 +53,9 @@ class GoogleResourceOwner extends GenericOAuth2ResourceOwner
     public function getAuthorizationUrl($redirectUri, array $extraParameters = array())
     {
         return parent::getAuthorizationUrl($redirectUri, array_merge(array(
-            'access_type' => $this->getOption('access_type'),
-            'approval_prompt' => $this->getOption('approval_prompt'),
-            'request_visible_actions' => $this->getOption('request_visible_options')
+            'access_type'             => $this->getOption('access_type'),
+            'approval_prompt'         => $this->getOption('approval_prompt'),
+            'request_visible_actions' => $this->getOption('request_visible_actions')
         ), $extraParameters));
     }
 }
