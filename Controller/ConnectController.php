b637b3649357858e383d8a4b2bcfd4ab6555c85d
Konstantin.Myakshin <koc-dp@yandex.ru>
2014-01-10 22:53:15 +0200

    Prevent SessionUnavailableException when returns back from service.

--- Controller/ConnectController.php
+++ Controller/ConnectController.php
@@ -238,9 +238,14 @@ class ConnectController extends ContainerAware
         // Check for a specified target path and store it before redirect if present
         $param = $this->container->getParameter('hwi_oauth.target_path_parameter');
 
-        if (!empty($param) && $request->hasSession() && $targetUrl = $request->get($param, null, true)) {
-            $providerKey = $this->container->getParameter('hwi_oauth.firewall_name');
-            $request->getSession()->set('_security.' . $providerKey . '.target_path', $targetUrl);
+        if ($request->hasSession()) {
+            // initialize the session for preventing SessionUnavailableException
+            $request->getSession()->start();
+
+            if (!empty($param) && $targetUrl = $request->get($param, null, true)) {
+                $providerKey = $this->container->getParameter('hwi_oauth.firewall_name');
+                $request->getSession()->set('_security.' . $providerKey . '.target_path', $targetUrl);
+            }
         }
 
         return new RedirectResponse($this->container->get('hwi_oauth.security.oauth_utils')->getAuthorizationUrl($request, $service));
