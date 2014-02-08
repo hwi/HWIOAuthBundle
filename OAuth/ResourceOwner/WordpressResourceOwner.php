d3ef4f7a2745a6a943d74ccfb38cdb41f5e291ac
ahmed-hamdy90 <thespy2030@yahoo.com>
2014-02-08 18:13:31 +0200

    solve Wordpress Bug into get user information

--- OAuth/ResourceOwner/WordpressResourceOwner.php
+++ OAuth/ResourceOwner/WordpressResourceOwner.php
@@ -12,11 +12,13 @@
 namespace HWI\Bundle\OAuthBundle\OAuth\ResourceOwner;
 
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use HWI\Bundle\OAuthBundle\Security\Core\Authentication\Token\OAuthToken;
 
 /**
  * WordpressResourceOwner
  *
  * @author Joseph Bielawski <stloyd@gmail.com>
+ * @author ahmed-hamdy90 <ahmedhamdy20@gmail.com>
  */
 class WordpressResourceOwner extends GenericOAuth2ResourceOwner
 {
@@ -30,6 +32,41 @@ class WordpressResourceOwner extends GenericOAuth2ResourceOwner
         'email'          => 'email',
         'profilepicture' => 'avatar_URL',
     );
+    
+    /**
+     * {@inheritDoc}
+     */
+    protected function doGetUserInformationRequest($url, array $parameters = array())
+    {
+        $accessToken = $parameters['accessToken'];
+        $headers = array(
+            0 => 'authorization: Bearer '.$accessToken,
+        );
+        return $this->httpRequest($url,null,$headers);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function getUserInformation(array $accessToken, array $extraParameters = array())
+    {
+        $url = $this->normalizeUrl($this->options['infos_url'], array(
+            'access_token' => $accessToken['access_token']
+        ));
+
+        $parameters = array(
+            'accessToken' => $accessToken['access_token'],
+        );
+        
+        $content = $this->doGetUserInformationRequest($url,$parameters)->getContent();
+
+        $response = $this->getUserResponse();
+        $response->setResponse($content);
+        $response->setResourceOwner($this);
+        $response->setOAuthToken(new OAuthToken($accessToken));
+     
+        return $response;
+    }
 
     /**
      * {@inheritDoc}
