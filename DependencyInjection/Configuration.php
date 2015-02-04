1685d3157fcbf5a6dc4d242cfc5a01b885fd7fe9
Baptiste Clavié <clavie.b@gmail.com>
2015-02-04 11:03:33 +0100

    Add Evernote resource owner

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -86,6 +86,7 @@ class Configuration implements ConfigurationInterface
             'bitbucket',
             'discogs',
             'dropbox',
+            'evernote',
             'flickr',
             'jira',
             'stereomood',
