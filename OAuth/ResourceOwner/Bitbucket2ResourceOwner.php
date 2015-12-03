d98993229a6a979065ff864f82fe94ecd8938e36
DSanchez <david38sanchez@gmail.com>
2015-12-03 17:15:15 -0500

    Add missing header and class comments

--- OAuth/ResourceOwner/Bitbucket2ResourceOwner.php
+++ OAuth/ResourceOwner/Bitbucket2ResourceOwner.php
@@ -1,8 +1,23 @@
 <?php
+
+/*
+ * This file is part of the HWIOAuthBundle package.
+ *
+ * (c) Hardware.Info <opensource@hardware.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace HWI\Bundle\OAuthBundle\OAuth\ResourceOwner;
 
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
+/**
+ * Bitbucket2ResourceOwner
+ *
+ * @author David Sanchez <david38sanchez@gmail.com>
+ */
 class Bitbucket2ResourceOwner extends GenericOAuth2ResourceOwner
 {
 
