884e88ba06fa11950549a62f71fa6525c60f9c70
DSanchez <david38sanchez@gmail.com>
2015-12-03 14:35:22 -0500

    Get email address, code was missing

--- OAuth/ResourceOwner/BitbucketResourceOwner.php
+++ OAuth/ResourceOwner/BitbucketResourceOwner.php
@@ -42,6 +42,36 @@ class BitbucketResourceOwner extends GenericOAuth1ResourceOwner
             'request_token_url' => 'https://bitbucket.org/api/1.0/oauth/request_token',
             'access_token_url'  => 'https://bitbucket.org/api/1.0/oauth/access_token',
             'infos_url'         => 'https://bitbucket.org/api/1.0/user',
+			'emails_url'		=> 'https://bitbucket.org/api/1.0/users/%s/emails'
         ));
     }
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getUserInformation(array $accessToken, array $extraParameters = array())
+	{
+		$response = parent::getUserInformation($accessToken, $extraParameters);
+
+		$responseData = $response->getResponse();
+
+		if (empty($responseData['email'])) {
+			// fetch the email addresses linked to the account
+			$content = $this->httpRequest($this->normalizeUrl(
+				sprintf($this->options['emails_url'], $responseData['username'])
+			), null, array('Authorization: Bearer '.$accessToken['access_token']));
+
+			foreach ($this->getResponseContent($content) as $email) {
+				if (!empty($email['primary'])) {
+					// we only need the primary email address
+					$responseData['email'] = $email['email'];
+					break;
+				}
+			}
+
+			$response->setResponse($responseData);
+		}
+
+		return $response;
+	}
 }
