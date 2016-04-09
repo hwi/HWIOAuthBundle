694cf22e5842b9c65cb0788810341ea7c5217e6d
Raynald <psyray@users.noreply.github.com>
2016-04-09 17:18:02 +0200

    Change Discogs URL from http to https
    According to this API announcement 25 days ago https://www.discogs.com/forum/thread/347984#7056344
    API must use https instead of http, or oAuth will not work

--- OAuth/ResourceOwner/DiscogsResourceOwner.php
+++ OAuth/ResourceOwner/DiscogsResourceOwner.php
@@ -22,10 +22,10 @@ class DiscogsResourceOwner extends GenericOAuth1ResourceOwner
         parent::configureOptions($resolver);
 
         $resolver->setDefaults(array(
-            'authorization_url' => 'http://www.discogs.com/oauth/authorize',
-            'request_token_url' => 'http://api.discogs.com/oauth/request_token',
-            'access_token_url'  => 'http://api.discogs.com/oauth/access_token',
-            'infos_url'         => 'http://api.discogs.com/oauth/identity',
+            'authorization_url' => 'https://www.discogs.com/oauth/authorize',
+            'request_token_url' => 'https://api.discogs.com/oauth/request_token',
+            'access_token_url'  => 'https://api.discogs.com/oauth/access_token',
+            'infos_url'         => 'https://api.discogs.com/oauth/identity',
         ));
     }
 } 
