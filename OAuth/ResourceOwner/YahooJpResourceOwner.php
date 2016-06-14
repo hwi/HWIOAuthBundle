51e818977041f582a1c30a8800e8b699fa6e6778
polidog <polidogs@gmail.com>
2016-06-15 01:54:54 +0900

    bug fix array

--- OAuth/ResourceOwner/YahooJpResourceOwner.php
+++ OAuth/ResourceOwner/YahooJpResourceOwner.php
@@ -30,9 +30,9 @@ class YahooJpResourceOwner extends GenericOAuth2ResourceOwner
     public function getUserInformation(array $accessToken, array $extraParameters = array())
     {
 
-        $content = $this->doGetUserInformationRequest($this->normalizeUrl($this->options['infos_url'], array('schema' => 'openid')),[
+        $content = $this->doGetUserInformationRequest($this->normalizeUrl($this->options['infos_url'], array('schema' => 'openid')),array(
             'access_token' => $accessToken['access_token']
-        ]);
+        ));
 
         $response = $this->getUserResponse();
         $response->setResponse($content->getContent());
