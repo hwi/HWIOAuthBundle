06ada38a399efba260a47ba0cc6db9dd628373ae
polidog <polidogs@gmail.com>
2016-06-15 01:43:10 +0900

    Add Test.

---
+++ Tests/OAuth/ResourceOwner/YahooJpResourceOwnerTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: polidog
+ * Date: 2016/06/15
+ */
+
+namespace HWI\Bundle\OAuthBundle\Tests\OAuth\ResourceOwner;
+
+
+use HWI\Bundle\OAuthBundle\OAuth\ResourceOwner\YahooJpResourceOwner;
+
+class YahooJpResourceOwnerTest extends GenericOAuth2ResourceOwnerTest
+{
+    protected $userResponse = <<<json
+{
+    "id": "1",
+    "name": "bar"
+}
+json;
+
+    protected $paths        = array(
+        'identifier'     => 'id',
+        'nickname'       => 'name',
+        'realname'       => 'name',
+    );
+
+    protected $expectedUrls = array(
+        'authorization_url'      => 'http://user.auth/?test=2&response_type=code&client_id=clientid&scope=openid+profile&redirect_uri=http%3A%2F%2Fredirect.to%2F',
+        'authorization_url_csrf' => 'http://user.auth/?test=2&response_type=code&client_id=clientid&scope=openid+profile&state=random&redirect_uri=http%3A%2F%2Fredirect.to%2F',
+    );
+
+
+    protected function setUpResourceOwner($name, $httpUtils, array $options)
+    {
+        return new YahooJpResourceOwner($this->buzzClient, $httpUtils, $options, $name, $this->storage);
+    }
+
+}
