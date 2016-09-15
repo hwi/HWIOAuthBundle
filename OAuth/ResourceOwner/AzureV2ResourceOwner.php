960ad54b6f080a598a0a0b8374d8a87ac526138c
Guillaume Potier <cobalt2760@gmail.com>
2016-09-15 18:04:22 +0200

    Azurev2 | Fix getUserInformation and paths

--- OAuth/ResourceOwner/AzureV2ResourceOwner.php
+++ OAuth/ResourceOwner/AzureV2ResourceOwner.php
@@ -29,11 +29,11 @@ class AzureV2ResourceOwner extends GenericOAuth2ResourceOwner
      * {@inheritDoc}
      */
     protected $paths = array(
-        'identifier'     => 'sub',
-        'nickname'       => 'preferred_username',
-        'realname'       => 'name',
-        'email'          => 'email',
-        'profilepicture' => null,
+        'identifier'    => 'id',
+        'email'         => 'mail',
+        'realname'      => 'displayName',
+        'firstName'     => 'givenName',
+        'lastName'      => 'surname'
     );
 
     /**
@@ -82,43 +82,6 @@ class AzureV2ResourceOwner extends GenericOAuth2ResourceOwner
     /**
      * {@inheritDoc}
      */
-    public function getUserInformation(array $accessToken, array $extraParameters = array())
-    {
-        // from http://stackoverflow.com/a/28748285/624544
-        list(, $jwt, ) = explode('.', $accessToken['id_token'], 3);
-
-        // if the token was urlencoded, do some fixes to ensure that it is valid base64 encoded
-        $jwt = str_replace('-', '+', $jwt);
-        $jwt = str_replace('_', '/', $jwt);
-
-        // complete token if needed
-        switch (strlen($jwt) % 4) {
-            case 0:
-                break;
-
-            case 2:
-                $jwt .= '=';
-
-            case 3:
-                $jwt .= '=';
-                break;
-
-            default:
-                throw new \InvalidArgumentException('Invalid base64 format sent back');
-        }
-
-        $response = $this->getUserResponse();
-        $response->setResponse(base64_decode($jwt));
-
-        $response->setResourceOwner($this);
-        $response->setOAuthToken(new OAuthToken($accessToken));
-
-        return $response;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     protected function configureOptions(OptionsResolver $resolver)
     {
         parent::configureOptions($resolver);
@@ -126,7 +89,7 @@ class AzureV2ResourceOwner extends GenericOAuth2ResourceOwner
         $resolver->setRequired(array('scope'));
 
         $resolver->setDefaults(array(
-            'infos_url' => '',
+            'infos_url' => 'https://graph.microsoft.com/v1.0/me',
             'authorization_url' => 'https://login.microsoftonline.com/%s/oauth2/%s/authorize',
             'access_token_url' => 'https://login.microsoftonline.com/%s/oauth2/%s/token',
             'application' => 'common',
