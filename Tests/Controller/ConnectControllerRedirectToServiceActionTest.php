18301200e3c8e05abfd5278b3d20d15cd3713581
Gerard Ruiz <gerard@mailtrack.io>
2016-11-29 18:55:26 +0100

    Adds test

--- Tests/Controller/ConnectControllerRedirectToServiceActionTest.php
+++ Tests/Controller/ConnectControllerRedirectToServiceActionTest.php
@@ -50,4 +50,19 @@ class ConnectControllerRedirectToServiceActionTest extends AbstractConnectContro
 
         $this->controller->redirectToServiceAction($this->request, 'facebook');
     }
+
+    public function testUnknownResourceOwner()
+    {
+        $this->oAuthUtils->expects($this->once())
+            ->method('getAuthorizationUrl')
+            ->with(
+                $this->isInstanceOf('Symfony\Component\HttpFoundation\Request'),
+                'unknown'
+            )
+            ->will($this->throwException(new \RuntimeException()))
+        ;
+
+        $this->expectException(\Symfony\Component\HttpKernel\Exception\NotFoundHttpException::class);
+        $this->controller->redirectToServiceAction($this->request, 'unknown');
+    }
 }
