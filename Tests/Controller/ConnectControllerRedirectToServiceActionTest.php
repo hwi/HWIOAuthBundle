f1da355292e0dc55e0bb2a2b02554aeeae0f22fa
Gerard Ruiz <gerard@mailtrack.io>
2016-11-30 11:25:43 +0100

    Makes expected exception assertion as annotation
    Tests were failing for php 5.3

--- Tests/Controller/ConnectControllerRedirectToServiceActionTest.php
+++ Tests/Controller/ConnectControllerRedirectToServiceActionTest.php
@@ -51,6 +51,9 @@ class ConnectControllerRedirectToServiceActionTest extends AbstractConnectContro
         $this->controller->redirectToServiceAction($this->request, 'facebook');
     }
 
+    /**
+     * @expectedException \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     */
     public function testUnknownResourceOwner()
     {
         $this->oAuthUtils->expects($this->once())
@@ -62,7 +65,6 @@ class ConnectControllerRedirectToServiceActionTest extends AbstractConnectContro
             ->will($this->throwException(new \RuntimeException()))
         ;
 
-        $this->expectException(\Symfony\Component\HttpKernel\Exception\NotFoundHttpException::class);
         $this->controller->redirectToServiceAction($this->request, 'unknown');
     }
 }
