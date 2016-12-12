1a03c967d218f9d18c4c8722c78472cb83647ed6
Joseph Bielawski <stloyd@gmail.com>
2016-12-12 11:39:31 +0100

    Add required scope for WindowsLive resource owner

--- Tests/OAuth/ResourceOwner/WindowsLiveResourceOwnerTest.php
+++ Tests/OAuth/ResourceOwner/WindowsLiveResourceOwnerTest.php
@@ -28,6 +28,11 @@ json;
         'realname'   => 'name',
     );
 
+    protected $expectedUrls = array(
+        'authorization_url'      => 'http://user.auth/?test=2&response_type=code&client_id=clientid&scope=wl.signin&redirect_uri=http%3A%2F%2Fredirect.to%2F',
+        'authorization_url_csrf' => 'http://user.auth/?test=2&response_type=code&client_id=clientid&scope=wl.signin&state=random&redirect_uri=http%3A%2F%2Fredirect.to%2F',
+    );
+
     protected function setUpResourceOwner($name, $httpUtils, array $options)
     {
         return new WindowsLiveResourceOwner($this->buzzClient, $httpUtils, $options, $name, $this->storage);
