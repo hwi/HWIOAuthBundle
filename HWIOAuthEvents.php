b242a435deed2dc92ffc7007aff4c5535b5d97a8
Konstantin.Myakshin <koc-dp@yandex.ru>
2017-05-08 18:09:44 +0300

    Mark event constants with annotation

--- HWIOAuthEvents.php
+++ HWIOAuthEvents.php
@@ -16,15 +16,33 @@ namespace HWI\Bundle\OAuthBundle;
  */
 final class HWIOAuthEvents
 {
+    /**
+     * @Event("HWI\Bundle\OAuthBundle\Event\GetResponseUserEvent")
+     */
     const REGISTRATION_INITIALIZE = 'hwi_oauth.registration.initialize';
 
+    /**
+     * @Event("HWI\Bundle\OAuthBundle\Event\FormEvent")
+     */
     const REGISTRATION_SUCCESS = 'hwi_oauth.registration.success';
 
+    /**
+     * @Event("HWI\Bundle\OAuthBundle\Event\GetResponseUserEvent")
+     */
     const REGISTRATION_COMPLETED = 'hwi_oauth.registration.completed';
 
+    /**
+     * @Event("HWI\Bundle\OAuthBundle\Event\GetResponseUserEvent")
+     */
     const CONNECT_INITIALIZE = 'hwi_oauth.connect.initialize';
 
+    /**
+     * @Event("HWI\Bundle\OAuthBundle\Event\GetResponseUserEvent")
+     */
     const CONNECT_CONFIRMED = 'hwi_oauth.connect.confirmed';
 
+    /**
+     * @Event("HWI\Bundle\OAuthBundle\Event\FilterUserResponseEvent")
+     */
     const CONNECT_COMPLETED = 'hwi_oauth.connect.completed';
 }
