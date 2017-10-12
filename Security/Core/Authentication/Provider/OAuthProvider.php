2676666637db0c8933d6f97b0ca44811e6a176bd
Ilir Hoxha <ilir.hoxha@smithhotels.com>
2017-10-12 14:30:35 +0100

    Adds functionality to refresh token if expired

--- Security/Core/Authentication/Provider/OAuthProvider.php
+++ Security/Core/Authentication/Provider/OAuthProvider.php
@@ -11,11 +11,13 @@
 
 namespace HWI\Bundle\OAuthBundle\Security\Core\Authentication\Provider;
 
+use HWI\Bundle\OAuthBundle\OAuth\ResourceOwnerInterface;
 use HWI\Bundle\OAuthBundle\Security\Core\Authentication\Token\OAuthToken;
 use HWI\Bundle\OAuthBundle\Security\Core\Exception\OAuthAwareExceptionInterface;
 use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthAwareUserProviderInterface;
 use HWI\Bundle\OAuthBundle\Security\Http\ResourceOwnerMapInterface;
 use Symfony\Component\Security\Core\Authentication\Provider\AuthenticationProviderInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -45,15 +47,22 @@ class OAuthProvider implements AuthenticationProviderInterface
     private $userChecker;
 
     /**
-     * @param OAuthAwareUserProviderInterface $userProvider     User provider
-     * @param ResourceOwnerMapInterface       $resourceOwnerMap Resource owner map
-     * @param UserCheckerInterface            $userChecker      User checker
+     * @var TokenStorageInterface
      */
-    public function __construct(OAuthAwareUserProviderInterface $userProvider, ResourceOwnerMapInterface $resourceOwnerMap, UserCheckerInterface $userChecker)
+    private $tokenStorage;
+
+    /**
+     * @param OAuthAwareUserProviderInterface $userProvider User provider
+     * @param ResourceOwnerMapInterface $resourceOwnerMap Resource owner map
+     * @param UserCheckerInterface $userChecker User checker
+     * @param TokenStorageInterface $tokenStorage
+     */
+    public function __construct(OAuthAwareUserProviderInterface $userProvider, ResourceOwnerMapInterface $resourceOwnerMap, UserCheckerInterface $userChecker, TokenStorageInterface $tokenStorage)
     {
         $this->userProvider = $userProvider;
         $this->resourceOwnerMap = $resourceOwnerMap;
         $this->userChecker = $userChecker;
+        $this->tokenStorage = $tokenStorage;
     }
 
     /**
@@ -64,7 +73,7 @@ class OAuthProvider implements AuthenticationProviderInterface
         return
             $token instanceof OAuthToken
             && $this->resourceOwnerMap->hasResourceOwnerByName($token->getResourceOwnerName())
-        ;
+            ;
     }
 
     /**
@@ -79,6 +88,7 @@ class OAuthProvider implements AuthenticationProviderInterface
         /* @var OAuthToken $token */
         $resourceOwner = $this->resourceOwnerMap->getResourceOwnerByName($token->getResourceOwnerName());
 
+        $token = $token->isExpired() ? $this->refreshToken($token, $resourceOwner) : $token;
         $userResponse = $resourceOwner->getUserInformation($token->getRawToken());
 
         try {
@@ -101,6 +111,21 @@ class OAuthProvider implements AuthenticationProviderInterface
         $token->setResourceOwnerName($resourceOwner->getName());
         $token->setUser($user);
         $token->setAuthenticated(true);
+
+        return $token;
+    }
+
+    /**
+     * @param TokenInterface $expiredToken
+     * @param ResourceOwnerInterface $resourceOwner
+     * @return OAuthToken|TokenInterface
+     */
+    protected function refreshToken(TokenInterface $expiredToken, ResourceOwnerInterface $resourceOwner)
+    {
+        $accessToken = $resourceOwner->refreshAccessToken($expiredToken->getRefreshToken());
+        $token = new OAuthToken($accessToken);
+        $token->setRefreshToken($expiredToken->getRefreshToken());
+        $this->tokenStorage->setToken($token);
 
         return $token;
     }
