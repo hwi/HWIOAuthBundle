128fb7446df929adef05a687b49ad6b9b541b290
Domenico Graziano <domenico.graziano@edo.co>
2017-11-14 10:45:44 +0000

    modified DropBoxResourceOwner to implement an a dropbox v2 and HttplugBundle complient getUserInformation method

--- OAuth/ResourceOwner/DropboxResourceOwner.php
+++ OAuth/ResourceOwner/DropboxResourceOwner.php
@@ -12,6 +12,7 @@
 namespace HWI\Bundle\OAuthBundle\OAuth\ResourceOwner;
 
 use HWI\Bundle\OAuthBundle\Security\Core\Authentication\Token\OAuthToken;
+use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -25,10 +26,10 @@ class DropboxResourceOwner extends GenericOAuth2ResourceOwner
      * {@inheritdoc}
      */
     protected $paths = array(
-        'identifier' => 'account_id',
-        'nickname' => 'email',
-        'realname' => 'email',
-        'email' => 'email',
+      'identifier' => 'account_id',
+      'nickname' => 'email',
+      'realname' => 'email',
+      'email' => 'email',
     );
 
     /**
@@ -39,30 +40,49 @@ class DropboxResourceOwner extends GenericOAuth2ResourceOwner
         parent::configureOptions($resolver);
 
         $resolver->setDefaults(array(
-            'authorization_url' => 'https://www.dropbox.com/oauth2/authorize',
-            'access_token_url' => 'https://api.dropbox.com/oauth2/token',
-            'infos_url' => 'https://api.dropboxapi.com/2/users/get_current_account',
+          'authorization_url' => 'https://www.dropbox.com/oauth2/authorize',
+          'access_token_url' => 'https://api.dropbox.com/oauth2/token',
+          'infos_url' => 'https://api.dropboxapi.com/2/users/get_current_account',
         ));
     }
 
-    public function getUserInformation(array $accessToken, array $extraParameters = array())
-    {
+    /**
+     *
+     * Dropbox API v2 requires a POST request to simply get user info
+     *
+     * @param array $accessToken
+     * @param array $extraParameters
+     * @return \HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface
+     */
+    public function getUserInformation(
+      array $accessToken,
+      array $extraParameters = array()
+    ) {
         if ($this->options['use_bearer_authorization']) {
-            $content = $this->httpRequest($this->normalizeUrl($this->options['infos_url'], $extraParameters), 'null',
-                array(
-                    'Content-type: application/json; charset=UTF-8',
-                    'Authorization: Bearer ' . $accessToken['access_token']
-                ), 'POST');
+            $content = $this->httpRequest(
+              $this->normalizeUrl($this->options['infos_url'],
+                $extraParameters),
+              'null',
+              array(
+                'Authorization' => 'Bearer ' . $accessToken['access_token'],
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json; charset=utf-8',
+              ), 'POST');
         } else {
-            $content = $this->doGetUserInformationRequest($this->normalizeUrl($this->options['infos_url'],
-                array_merge(array($this->options['attr_name'] => $accessToken['access_token']), $extraParameters)));
+            $content = $this->doGetUserInformationRequest(
+              $this->normalizeUrl(
+                $this->options['infos_url'],
+                array_merge(array($this->options['attr_name'] => $accessToken['access_token']),
+                  $extraParameters)
+              )
+            );
         }
 
         $response = $this->getUserResponse();
-        $response->setResponse($content->getContent());
-
+        $response->setData($content instanceof ResponseInterface ? (string)$content->getBody() : $content);
         $response->setResourceOwner($this);
         $response->setOAuthToken(new OAuthToken($accessToken));
         return $response;
+
     }
 }
