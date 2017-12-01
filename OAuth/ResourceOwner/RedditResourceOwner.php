7fcd8aa2e5ff3c2087bbee860c740f2f23b29d85
just-an-dev <just-an-dev@ggcorp.fr>
2017-12-01 03:35:03 +0100

    fix an error on header

--- OAuth/ResourceOwner/RedditResourceOwner.php
+++ OAuth/ResourceOwner/RedditResourceOwner.php
@@ -37,7 +37,7 @@ class RedditResourceOwner extends GenericOAuth2ResourceOwner
             $url,
             http_build_query($parameters, '', '&'),
             [
-                'Authorization: Basic '.base64_encode(sprintf('%s:%s', $this->options['client_id'], $this->options['client_secret'])),
+                'Authorization' => 'Basic '.base64_encode(sprintf('%s:%s', $this->options['client_id'], $this->options['client_secret'])),
             ],
             'POST'
         );
