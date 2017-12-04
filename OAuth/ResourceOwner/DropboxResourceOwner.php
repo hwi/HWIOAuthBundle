d0aa71b1a879b4928ab28d279efb2c928c07aa6d
domgraziano <iDoM@iMacDoM-2.local>
2017-12-04 21:24:24 +0000

    fixed coding standards and other CI errors reported by Soullivaneuh

--- OAuth/ResourceOwner/DropboxResourceOwner.php
+++ OAuth/ResourceOwner/DropboxResourceOwner.php
@@ -12,6 +12,7 @@
 namespace HWI\Bundle\OAuthBundle\OAuth\ResourceOwner;
 
 use HWI\Bundle\OAuthBundle\Security\Core\Authentication\Token\OAuthToken;
+use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
 use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -26,10 +27,10 @@ class DropboxResourceOwner extends GenericOAuth2ResourceOwner
      * {@inheritdoc}
      */
     protected $paths = array(
-      'identifier' => 'account_id',
-      'nickname' => 'email',
-      'realname' => 'email',
-      'email' => 'email',
+        'identifier' => 'account_id',
+        'nickname' => 'email',
+        'realname' => 'email',
+        'email' => 'email',
     );
 
     /**
@@ -40,41 +41,39 @@ class DropboxResourceOwner extends GenericOAuth2ResourceOwner
         parent::configureOptions($resolver);
 
         $resolver->setDefaults(array(
-          'authorization_url' => 'https://www.dropbox.com/oauth2/authorize',
-          'access_token_url' => 'https://api.dropbox.com/oauth2/token',
-          'infos_url' => 'https://api.dropboxapi.com/2/users/get_current_account',
+            'authorization_url' => 'https://www.dropbox.com/oauth2/authorize',
+            'access_token_url' => 'https://api.dropbox.com/oauth2/token',
+            'infos_url' => 'https://api.dropboxapi.com/2/users/get_current_account',
         ));
     }
 
     /**
-     *
      * Dropbox API v2 requires a POST request to simply get user info
      *
      * @param array $accessToken
      * @param array $extraParameters
-     * @return \HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface
+     * @return UserResponseInterface
      */
-    public function getUserInformation(
-      array $accessToken,
-      array $extraParameters = array()
+    public function getUserInformation(array $accessToken,
+        array $extraParameters = array()
     ) {
         if ($this->options['use_bearer_authorization']) {
             $content = $this->httpRequest(
-              $this->normalizeUrl($this->options['infos_url'],
-                $extraParameters),
-              'null',
-              array(
-                'Authorization' => 'Bearer ' . $accessToken['access_token'],
-                'Accept' => 'application/json',
-                'Content-Type' => 'application/json; charset=utf-8',
-              ), 'POST');
+                $this->normalizeUrl($this->options['infos_url'],
+                    $extraParameters),
+                'null',
+                array(
+                    'Authorization' => 'Bearer ' . $accessToken['access_token'],
+                    'Accept' => 'application/json',
+                    'Content-Type' => 'application/json; charset=utf-8',
+                ), 'POST');
         } else {
             $content = $this->doGetUserInformationRequest(
-              $this->normalizeUrl(
-                $this->options['infos_url'],
-                array_merge(array($this->options['attr_name'] => $accessToken['access_token']),
-                  $extraParameters)
-              )
+                $this->normalizeUrl(
+                    $this->options['infos_url'],
+                    array_merge(array($this->options['attr_name'] => $accessToken['access_token']),
+                        $extraParameters)
+                )
             );
         }
 
@@ -82,7 +81,7 @@ class DropboxResourceOwner extends GenericOAuth2ResourceOwner
         $response->setData($content instanceof ResponseInterface ? (string)$content->getBody() : $content);
         $response->setResourceOwner($this);
         $response->setOAuthToken(new OAuthToken($accessToken));
-        return $response;
 
+        return $response;
     }
 }
