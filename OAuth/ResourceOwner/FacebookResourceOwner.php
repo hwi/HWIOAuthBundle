203598a8ea562d33df11762f843c442c636e8e73
Guillaume RODRIGUEZ <g.rodriguez@opendecide.com>
2018-06-22 22:50:13 +0200

    Reverse Facebook API Migration

--- OAuth/ResourceOwner/FacebookResourceOwner.php
+++ OAuth/ResourceOwner/FacebookResourceOwner.php
@@ -112,10 +112,10 @@ class FacebookResourceOwner extends GenericOAuth2ResourceOwner
         parent::configureOptions($resolver);
 
         $resolver->setDefaults(array(
-            'authorization_url' => 'https://www.facebook.com/v3.0/dialog/oauth',
-            'access_token_url' => 'https://graph.facebook.com/v3.0/oauth/access_token',
-            'revoke_token_url' => 'https://graph.facebook.com/v3.0/me/permissions',
-            'infos_url' => 'https://graph.facebook.com/v3.0/me?fields=id,first_name,last_name,name,email',
+            'authorization_url' => 'https://www.facebook.com/v2.8/dialog/oauth',
+            'access_token_url' => 'https://graph.facebook.com/v2.8/oauth/access_token',
+            'revoke_token_url' => 'https://graph.facebook.com/v2.8/me/permissions',
+            'infos_url' => 'https://graph.facebook.com/v2.8/me?fields=id,first_name,last_name,name,email',
             'use_commas_in_scope' => true,
             'display' => null,
             'auth_type' => null,
