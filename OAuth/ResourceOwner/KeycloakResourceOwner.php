d2656d70383a6ef9e2dad0890233e3e516a1885a
Dirk39 <andreaquin1990@gmail.com>
2019-01-18 16:35:42 +0100

    pass test to generate authentication URL

--- OAuth/ResourceOwner/KeycloakResourceOwner.php
+++ OAuth/ResourceOwner/KeycloakResourceOwner.php
@@ -17,14 +17,45 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class KeycloakResourceOwner extends GenericOAuth2ResourceOwner
 {
+    public function configure()
+    {
+        $this->prepareBaseAuthenticationUrl();
+    }
+
+    public function getAuthorizationUrl($redirectUri, array $extraParameters = array())
+    {
+        return parent::getAuthorizationUrl($redirectUri, array_merge([
+          'approval_prompt' => $this->getOption('approval_prompt')
+        ],$extraParameters));
+    }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         parent::configureOptions($resolver);
 
         $resolver
+          ->setDefined(['protocol', 'response_type', 'approval_prompt'])
           ->setRequired('realms')
-          ->setRequired('protocol')
-          ->setDefault('protocol', 'openid-connect');
+          ->setDefaults([
+            'protocol' => 'openid-connect',
+            'scope' =>  'name,email',
+            'response_type' => 'code',
+            'approval_prompt' => 'auto'
+          ]);
+    }
+
+    protected function prepareBaseAuthenticationUrl()
+    {
+        $baseAuthUrl = trim($this->getOption('authorization_url'), '/');
+        //check if already configured
+        if(strpos($baseAuthUrl, '/realms') !== false) {
+            return;
+        }
+
+        $baseAuthUrl .= '/realms/'.$this->getOption('realms');
+        $baseAuthUrl .= '/protocol/'.$this->getOption('protocol').'/auth';
+
+        $this->options['authorization_url'] = $baseAuthUrl;
+
     }
 }
--- Tests/OAuth/ResourceOwner/KeycloakResourceOwnerTest.php
+++ Tests/OAuth/ResourceOwner/KeycloakResourceOwnerTest.php
@@ -12,5 +12,22 @@ use HWI\Bundle\OAuthBundle\OAuth\ResourceOwner\KeycloakResourceOwner;
 
 class KeycloakResourceOwnerTest extends GenericOAuth2ResourceOwnerTest
 {
+    protected $options = array(
+      'client_id' => 'clientid',
+      'client_secret' => 'clientsecret',
+      'realms' => 'example',
+
+      'infos_url' => 'http://keycloak.info/auth',
+      'authorization_url' => 'http://keycloak.auth/auth',
+      'access_token_url' => 'http://keycloak.auth/auth',
+
+      'attr_name' => 'access_token',
+    );
+
+    protected $expectedUrls = array(
+      'authorization_url' => 'http://keycloak.auth/auth/realms/example/protocol/openid-connect/auth?response_type=code&client_id=clientid&scope=name%2Cemail&redirect_uri=http%3A%2F%2Fredirect.to%2F&approval_prompt=auto',
+      'authorization_url_csrf' => 'http://keycloak.auth/auth/realms/example/protocol/openid-connect/auth?response_type=code&client_id=clientid&scope=name%2Cemail&state=random&redirect_uri=http%3A%2F%2Fredirect.to%2F&approval_prompt=auto',
+    );
+
     protected $resourceOwnerClass = KeycloakResourceOwner::class;
 }
