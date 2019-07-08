8e98d66e75e1eac71e4e526049ad7d0bbd3ead67
Alessandro Lai <alessandro.lai85@gmail.com>
2019-07-08 12:35:02 +0200

    Add second integration test; mock the HTTP client; fix CS

--- Tests/App/AppKernel.php
+++ Tests/App/AppKernel.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the HWIOAuthBundle package.
+ *
+ * (c) Hardware.Info <opensource@hardware.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace HWI\Bundle\OAuthBundle\Tests\App;
 
 use Symfony\Component\Config\Loader\LoaderInterface;
@@ -24,12 +33,12 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader): void
     {
-        $loader->load(__DIR__ . '/config.yml');
+        $loader->load(__DIR__.'/config.yml');
     }
 
     public function getCacheDir(): string
     {
-        return $this->getBaseDir() . 'cache';
+        return $this->getBaseDir().'cache';
     }
 
     public function getProjectDir(): string
@@ -39,11 +48,11 @@ class AppKernel extends Kernel
 
     public function getLogDir(): string
     {
-        return $this->getBaseDir() . 'log';
+        return $this->getBaseDir().'log';
     }
 
     protected function getBaseDir(): string
     {
-        return sys_get_temp_dir() . '/hwioauth-bundle/' . (new \ReflectionClass($this))->getShortName() . '/var/';
+        return sys_get_temp_dir().'/hwioauth-bundle/'.(new \ReflectionClass($this))->getShortName().'/var/';
     }
 }
