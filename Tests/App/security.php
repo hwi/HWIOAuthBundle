0efcc6eee55ef8535c734594ab715d4c57118c59
Alessandro Lai <alessandro.lai85@gmail.com>
2019-07-09 09:15:54 +0200

    Fix deprecation

---
+++ Tests/App/security.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the HWIOAuthBundle package.
+ *
+ * (c) Hardware.Info <opensource@hardware.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Controller\UserValueResolver;
+
+if (method_exists(Security::class, 'getUser') && !class_exists(UserValueResolver::class)) {
+    $container->loadFromExtension('security', [
+        'firewalls' => [
+            'main' => [
+                'logout_on_user_change' => true,
+            ],
+        ],
+    ]);
+}
