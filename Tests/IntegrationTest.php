7a13716e68faf185bd22aef20b37a523569f2ea3
XWB <karelke@gmail.com>
2019-08-16 22:08:18 +0200

    Brand name update

--- Tests/IntegrationTest.php
+++ Tests/IntegrationTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of the HWIOAuthBundle package.
  *
- * (c) Hardware.Info <opensource@hardware.info>
+ * (c) Hardware Info <opensource@hardware.info>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
