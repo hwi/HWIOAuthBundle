2f0109f61ec5d07e91b57bf6f3f93dd30558e159
Fabiano Roberto <fabiano.roberto@ped.technology>
2019-10-24 11:50:47 +0200

    Refactor InstagramResourceOwner compliant with Instagram Basic Display API

--- OAuth/ResourceOwner/InstagramResourceOwner.php
+++ OAuth/ResourceOwner/InstagramResourceOwner.php
@@ -11,12 +11,14 @@
 
 namespace HWI\Bundle\OAuthBundle\OAuth\ResourceOwner;
 
+use HWI\Bundle\OAuthBundle\Security\OAuthErrorHandler;
+use Symfony\Component\HttpFoundation\Request as HttpRequest;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * InstagramResourceOwner.
  *
- * @author Jean-Christophe Cuvelier <jcc@atomseeds.com>
+ * @author Fabiano Roberto <fabiano.roberto@ped.technology>
  */
 class InstagramResourceOwner extends GenericOAuth2ResourceOwner
 {
@@ -24,10 +26,11 @@ class InstagramResourceOwner extends GenericOAuth2ResourceOwner
      * {@inheritdoc}
      */
     protected $paths = array(
-        'identifier' => 'data.id',
-        'nickname' => 'data.username',
-        'realname' => 'data.full_name',
-        'profilepicture' => 'data.profile_picture',
+        'identifier' => 'id',
+        'nickname' => 'username',
+        'realname' => 'username',
+        'email' => 'id',
+        'accounttype' => 'account_type',
     );
 
     /**
@@ -41,6 +44,53 @@ class InstagramResourceOwner extends GenericOAuth2ResourceOwner
     /**
      * {@inheritdoc}
      */
+    public function getAuthorizationUrl($redirectUri, array $extraParameters = array())
+    {
+        if ($this->options['csrf']) {
+            if (null === $this->state) {
+                $this->state = $this->generateNonce();
+            }
+
+            $this->storage->save($this, $this->state, 'csrf_state');
+        }
+
+        $parameters = array_merge(array(
+            'response_type' => 'code',
+            'app_id' => $this->options['client_id'],
+            'scope' => $this->options['scope'],
+            'state' => $this->state ? urlencode($this->state) : null,
+            'redirect_uri' => $redirectUri,
+        ), $extraParameters);
+
+        return $this->normalizeUrl($this->options['authorization_url'], $parameters);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAccessToken(HttpRequest $request, $redirectUri, array $extraParameters = array())
+    {
+        OAuthErrorHandler::handleOAuthError($request);
+
+        $parameters = array_merge(array(
+            'code' => $request->query->get('code'),
+            'grant_type' => 'authorization_code',
+            'app_id' => $this->options['client_id'],
+            'app_secret' => $this->options['client_secret'],
+            'redirect_uri' => $redirectUri,
+        ), $extraParameters);
+
+        $response = $this->doGetTokenRequest($this->options['access_token_url'], $parameters);
+        $response = $this->getResponseContent($response);
+
+        $this->validateResponseContent($response);
+
+        return $response;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     protected function configureOptions(OptionsResolver $resolver)
     {
         parent::configureOptions($resolver);
@@ -48,12 +98,7 @@ class InstagramResourceOwner extends GenericOAuth2ResourceOwner
         $resolver->setDefaults(array(
             'authorization_url' => 'https://api.instagram.com/oauth/authorize',
             'access_token_url' => 'https://api.instagram.com/oauth/access_token',
-            'infos_url' => 'https://api.instagram.com/v1/users/self',
-
-            // Instagram supports authentication with only one defined URL
-            'auth_with_one_url' => true,
-
-            'use_bearer_authorization' => false,
+            'infos_url' => 'https://graph.instagram.com/me?fields=id,username,account_type',
         ));
     }
 }
