8e6452daf970eef783621a9521e204f1977f80a8
Martin Wind <martin.wind@im-c.at>
2020-04-09 11:38:07 +0200

    Fix azure tests

--- Tests/OAuth/ResourceOwner/AzureResourceOwnerTest.php
+++ Tests/OAuth/ResourceOwner/AzureResourceOwnerTest.php
@@ -41,13 +41,13 @@ json;
     ];
 
     protected $expectedUrls = [
-        'authorization_url_csrf' => 'http://user.auth/?test=2&response_type=code&client_id=clientid&state=random&redirect_uri=http%3A%2F%2Fredirect.to%2F&resource=https%3A%2F%2Fgraph.windows.net',
+        'authorization_url_csrf' => 'http://user.auth/?test=2&response_type=code&client_id=clientid&state=random&redirect_uri=http%3A%2F%2Fredirect.to%2F',
     ];
 
     public function testGetAuthorizationUrl()
     {
         $this->assertEquals(
-            $this->options['authorization_url'].'&response_type=code&client_id=clientid&state=random&redirect_uri=http%3A%2F%2Fredirect.to%2F&resource=https%3A%2F%2Fgraph.windows.net',
+            $this->options['authorization_url'].'&response_type=code&client_id=clientid&state=random&redirect_uri=http%3A%2F%2Fredirect.to%2F',
             $this->resourceOwner->getAuthorizationUrl('http://redirect.to/')
         );
     }
@@ -109,19 +109,5 @@ json;
         } catch (HttpTransportException $e) {
             $this->assertSame($exception, $e->getPrevious());
         }
-    }
-
-    protected function setUpResourceOwner($name, HttpUtils $httpUtils, array $options)
-    {
-        return parent::setUpResourceOwner(
-            $name,
-            $httpUtils,
-            array_merge(
-                [
-                    'resource' => 'https://graph.windows.net',
-                ],
-                $options
-            )
-        );
     }
 }
