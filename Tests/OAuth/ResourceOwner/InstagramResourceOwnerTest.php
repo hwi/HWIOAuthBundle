477983eac881f826ea9db8b898b7ccf889e2e1d6
Paul Radt <paul.radt@spinninrecords.nl>
2020-06-18 13:19:08 +0200

    Fix #1636

--- Tests/OAuth/ResourceOwner/InstagramResourceOwnerTest.php
+++ Tests/OAuth/ResourceOwner/InstagramResourceOwnerTest.php
@@ -29,8 +29,8 @@ json;
     ];
 
     protected $expectedUrls = [
-        'authorization_url' => 'http://user.auth/?test=2&response_type=code&app_id=clientid&redirect_uri=http%3A%2F%2Fredirect.to%2F',
-        'authorization_url_csrf' => 'http://user.auth/?test=2&response_type=code&app_id=clientid&state=random&redirect_uri=http%3A%2F%2Fredirect.to%2F',
+        'authorization_url' => 'http://user.auth/?test=2&response_type=code&client_id=clientid&redirect_uri=http%3A%2F%2Fredirect.to%2F',
+        'authorization_url_csrf' => 'http://user.auth/?test=2&response_type=code&client_id=clientid&state=random&redirect_uri=http%3A%2F%2Fredirect.to%2F',
     ];
 
     public function testCustomResponseClass()
