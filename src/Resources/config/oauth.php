13494094334a0c670815b3d2b7bd993a22f292c4
Joseph Bielawski <stloyd@gmail.com>
2024-02-23 12:13:09 +0100

    Remove Twig usage from `AuthenticationFailureHandler`

--- src/Resources/config/oauth.php
+++ src/Resources/config/oauth.php
@@ -65,7 +65,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->args([
             service('request_stack'),
             service('router'),
-            service('twig'),
             '%hwi_oauth.connect%',
         ]);
 };
--- src/Security/Http/Authentication/AuthenticationFailureHandler.php
+++ src/Security/Http/Authentication/AuthenticationFailureHandler.php
@@ -19,24 +19,44 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
-use Twig\Environment;
+use Symfony\Component\Security\Http\ParameterBagUtils;
 
 final class AuthenticationFailureHandler implements AuthenticationFailureHandlerInterface
 {
+    private array $defaultOptions = [
+        'failure_path' => 'hwi_oauth_connect_registration',
+        'failure_forward' => false,
+        'login_path' => '/login',
+        'failure_path_parameter' => '_failure_path',
+    ];
+
     public function __construct(
         private readonly RequestStack $requestStack,
         private readonly RouterInterface $router,
-        private readonly Environment $twig,
-        private readonly bool $connect
+        private readonly bool $connect,
     ) {
+    }
+
+    public function setOptions(array $options): void
+    {
+        $this->defaultOptions = array_merge($this->defaultOptions, $options);
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
     {
+        $options = $this->defaultOptions;
+
+        $failureUrl = ParameterBagUtils::getRequestParameterValue($request, $options['failure_path_parameter']);
+
+        if (\is_string($failureUrl) && (str_starts_with($failureUrl, '/') || str_starts_with($failureUrl, 'http'))) {
+            $options['failure_path'] = $failureUrl;
+        }
+
+        $options['failure_path'] ??= $options['login_path'];
+
         $error = $exception->getPrevious();
 
         if ($this->connect && $error instanceof AccountNotLinkedException) {
@@ -50,9 +70,13 @@ final class AuthenticationFailureHandler implements AuthenticationFailureHandler
                 $session->set('_hwi_oauth.registration_error.'.$key, $error);
             }
 
-            return new RedirectResponse(
-                $this->router->generate('hwi_oauth_connect_registration', ['key' => $key], UrlGeneratorInterface::ABSOLUTE_PATH)
-            );
+            if ('/' === $options['failure_path'][0]) {
+                $failurePath = $request->getUriForPath($options['failure_path'][0]);
+            } else {
+                $failurePath = $this->router->generate($options['failure_path'], ['key' => $key]);
+            }
+
+            return new RedirectResponse($failurePath);
         }
 
         if ($error instanceof AuthenticationException) {
@@ -61,9 +85,13 @@ final class AuthenticationFailureHandler implements AuthenticationFailureHandler
             $error = $exception->getMessageKey();
         }
 
-        return new Response(
-            $this->twig->render('@HWIOAuth/Connect/login.html.twig', ['error' => $error])
-        );
+        if ('/' === $options['login_path'][0]) {
+            $loginPath = $request->getUriForPath($options['login_path'][0]);
+        } else {
+            $loginPath = $this->router->generate($options['login_path'], ['error' => $error]);
+        }
+
+        return new RedirectResponse($loginPath);
     }
 
     private function getSession(): ?SessionInterface
