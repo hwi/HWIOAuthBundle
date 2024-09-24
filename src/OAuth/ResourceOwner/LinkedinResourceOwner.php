35ad56d14fa81c189b23fe9f7be10a6ead4a4d8e
Alexis Vachet <129440693+Alx78370@users.noreply.github.com>
2024-09-24 11:58:28 +0200

    fix Linkedin info URL

--- src/OAuth/ResourceOwner/LinkedinResourceOwner.php
+++ src/OAuth/ResourceOwner/LinkedinResourceOwner.php
@@ -100,7 +100,7 @@ final class LinkedinResourceOwner extends GenericOAuth2ResourceOwner
             'scope' => 'r_liteprofile r_emailaddress',
             'authorization_url' => 'https://www.linkedin.com/oauth/v2/authorization',
             'access_token_url' => 'https://www.linkedin.com/oauth/v2/accessToken',
-            'infos_url' => 'https://api.linkedin.com/v2/me?projection=(id,firstName,lastName,profilePicture(displayImage~:playableStreams))',
+            'infos_url' => 'https://api.linkedin.com/v2/userinfo',
             'email_url' => 'https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))',
 
             'user_response_class' => LinkedinUserResponse::class,
